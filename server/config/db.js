cefbb7e5f5ac0398c0bd37c286c16b69e95feae8
Yamini <97643102+Yamini650@users.noreply.github.com>
2022-03-07 01:05:16 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  
+  "URI": "mongodb+srv://yamini650:RHPuh13Bzv2gqpfQ@booklistcluster.f7wdp.mongodb.net/books_store?retryWrites=true&w=majority"
+};
